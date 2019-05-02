b14ffef8500fb8740f980e4a3a4028cb23aa815c
mityon <mitera0104@gmail.com>
2019-05-02 12:12:34 +0900

    alias設定をファイル分割

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 ln -sf ~/dotfiles/_vimrc ~/.vimrc
 ln -sf ~/dotfiles/_zshrc ~/.zshrc
 ln -sf ~/dotfiles/_tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/zsh.d ~/.zsh.d
 
 if [ "$(uname)" == 'Darwin' ]; then
   echo "mac"
