c5d36f047caf0ebf7e80bf12a18f89bab8e3fb3d
mihiron <mihiron@xxxxx.xxxxx.com>
2026-07-12 08:58:27 +0900

    keybind追加

--- home/zsh/conf.d/keybind.zsh
+++ home/zsh/conf.d/keybind.zsh
@@ -4,10 +4,18 @@
 bindkey -v
 bindkey '^R' history-incremental-search-backward
 
+autoload -Uz history-search-end
+
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+
+## Search history entries that start with the current input.
+bindkey '^P' history-beginning-search-backward-end
+bindkey '^N' history-beginning-search-forward-end
+
 autoload -Uz select-word-style
 select-word-style default
 
 ## Treat / as a separator so Ctrl-W can delete one path segment at a time.
 zstyle ':zle:*' word-chars " /=;@:{},|"
 zstyle ':zle:*' word-style unspecified
-
